ac164c5a3a912006892d758f1ec279b4784822f1
Muhammad Rijal Aditya <136682708+Rijal0321@users.noreply.github.com>
2024-11-08 15:49:03 +0800

    Menambahkan Import swing dan awt

--- src/CekGanjilGenapFrame.java
+++ src/CekGanjilGenapFrame.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+import javax.swing.*;
+import java.awt.event.*;
 
-/**
- *
- * @author User
- */
 public class CekGanjilGenapFrame extends javax.swing.JFrame {
 
     /**
